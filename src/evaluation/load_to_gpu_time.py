eedb4a338582553f5fccaffc7496dc32ace0fcd5
Kamil Kojs <kamil.kojs@gmail.com>
2024-05-09 11:25:30 +0200

    wip

---
+++ src/evaluation/load_to_gpu_time.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import time
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from PIL import Image
+
+model = models.resnet152()
+model.load_state_dict(torch.load(f'resnet152_weights_1.pth'))
+model.eval()
+
+start_time = time.time()
+model.to("cuda:0")
+end_time = time.time()
+print(f"time to cuda: {end_time-start_time}")
+print(torch.cuda.memory_allocated())
+
+start_time = time.time()
+del model
+end_time = time.time()
+print(f"time deletion: {end_time-start_time}")
+print(torch.cuda.memory_allocated())
